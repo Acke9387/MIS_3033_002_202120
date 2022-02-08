4ff8d3aca1c48b0af03e504fa970b5dd245a89bc
Acke9387 <adam@ou.edu>
2022-02-08 13:13:45 -0600

    Completed in class example over reading data and stuff
    went over nuget
    finished participation

---
+++ In_Class_Examples/WPF-ReadingFilesAndProcessingData/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ReadingFilesAndProcessingData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<SalesDataItem> Data = new List<SalesDataItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] linesOfFile = File.ReadAllLines("SalesJan2009.csv");
+            for (int i = 1; i < linesOfFile.Length; i++)
+            {
+                //        0           1      2        3         4   5     6      7            8           9         10       11
+                //Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
+
+                //string[] pieces = linesOfFile[i].Split(',');
+                //SalesDataItem item = new SalesDataItem();
+                //item.Transaction_date = Convert.ToDateTime(pieces[0]);
+                //item.Product = pieces[1];
+
+                SalesDataItem item = new SalesDataItem(linesOfFile[i]);
+                if (cboPaymentType.Items.Contains(item.Payment_Type) == false)
+                {
+                    cboPaymentType.Items.Add(item.Payment_Type);
+                }
+                lstSalesData.Items.Add(item);
+                Data.Add(item);
+            }
+
+
+
+        }
+
+        private void cboPaymentType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string paymentType = cboPaymentType.SelectedItem.ToString();
+            lstSalesData.Items.Clear();
+
+            foreach (SalesDataItem item in Data)
+            {
+                if (item.Payment_Type == paymentType)
+                {
+                    lstSalesData.Items.Add(item);
+                }
+            }
+        }
+
+        private void lstSalesData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstSalesData.SelectedItem is null)
+            {
+                return;
+            }
+            SalesDataItem selectedItem = (SalesDataItem) lstSalesData.SelectedItem;
+
+            MessageBox.Show($"That transaction occurred on {selectedItem.Transaction_date}");
+        }
+    }
+}
---
+++ In_Class_Examples/WPF-ReadingFilesAndProcessingData/SalesDataItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ReadingFilesAndProcessingData
+{
+    internal class SalesDataItem
+    {
+        public DateTime Transaction_date { get; set; }
+        public string Product { get; set; }
+        public double Price { get; set; }
+        public string Payment_Type { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Account_Created { get; set; }
+        public string Last_Login { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+        public SalesDataItem()
+        {
+            Transaction_date = DateTime.Now;
+            Product = string.Empty;
+            Price = 0;
+            Payment_Type = string.Empty;
+            Name = string.Empty;
+            City = string.Empty;
+            State = string.Empty;
+            Country = string.Empty;
+            Account_Created = string.Empty;
+            Last_Login = string.Empty;
+            Latitude = string.Empty;
+            Longitude = string.Empty;
+        }
+
+        public SalesDataItem(string stuff)
+        {
+            //        0           1      2        3         4   5     6      7            8           9         10       11
+            //Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
+
+            string[] pieces = stuff.Split(',');
+            DateTime temp;
+            if (DateTime.TryParse(pieces[0], out temp))
+            {
+                Transaction_date = temp; 
+            }
+            else
+            {
+                Transaction_date = DateTime.Now;
+            }
+            Product = pieces[1].Trim();
+            Price = Convert.ToDouble(pieces[2]);
+            Payment_Type = pieces[3].Trim();
+            Name = pieces[4].Trim();
+            City = pieces[5].Trim();
+            State = pieces[6].Trim();
+            Country = pieces[7];
+            Account_Created = pieces[8];
+            Last_Login = pieces[9];
+            Latitude = pieces[10];
+            Longitude = pieces[11];
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Product} cost {Price.ToString("C")} and was bought by {Name} using {Payment_Type}.";
+        }
+    }
+}
