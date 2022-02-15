fe87a78c2fe8609d7a26c160a230a46a31d1061a
Acke9387 <adam@ou.edu>
2022-02-15 13:08:51 -0600

    did an in class example over JSON

---
+++ In_Class_Examples/WPF-JSON/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_JSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string json = File.ReadAllText("MOCK_DATA.json");
+
+            List<Person> ps = JsonConvert.DeserializeObject< List<Person>>(json);
+
+            //lstPeeps.ItemsSource = ps;
+
+            foreach (Person p in ps)
+            {
+                if (p.city.ToLower()[0] == 'p')
+                {
+                    lstPeeps.Items.Add(p); 
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string exportData = JsonConvert.SerializeObject(lstPeeps.Items, Formatting.Indented);
+
+            File.WriteAllText("filteredData.json", exportData);
+            
+            MessageBox.Show("Saved!");
+        }
+    }
+}
---
+++ In_Class_Examples/WPF-JSON/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_JSON
+{
+    public class Person
+    {
+        public int id { get; set; }
+
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public string email { get; set; }
+
+        public string  city { get; set; }
+
+        public override string ToString()
+        {
+            return $"{first_name} {last_name} - {city}";
+        }
+
+        public Person()
+        {
+            id = 0;
+            first_name = string.Empty;
+            last_name = string.Empty;
+            email = string.Empty;   
+            city = string.Empty;    
+        }
+    }
+}
